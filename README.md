ed6a67305c64714e96142756c346b35e0c9373f8
gildean <gildean@salaliitto.com>
2012-12-16 11:03:36 +0200

    Add multigame option

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ by: ok 2012
 
 Simple eventful dice for rpg or any dicegame.
 
+Designed to support multiple games and players using the same dice, for example in multiple socket.io rooms.
+
 Usage
 -----
 Install the module:
@@ -14,18 +16,18 @@ Require the module:
 `var dice = require('rpgdice');`
 
 Roll the dice:
-`dice.roll('1d6+4', 'player1')`
-*the modifier (+4 etc.) and the playername can be omitted*
+`dice.roll('1d6+4', 'player1', 'game1')`
+*the modifier (+4 etc.), the playername and the gamename can be omitted*
 
 Dice has the following events:
 
-`dice.on('rolling', function (dice, roller) {});` *when a player rolls*
+`dice.on('rolling', function (dice, roller, game) {});` *when a player rolls*
 
-`dice.on('roll', function (roll, roller) {});` *when a single dice rolls*
+`dice.on('roll', function (roll, roller, game) {});` *when a single dice rolls*
 
-`dice.on('result', function (result, roller) {});` *when all the dice have been rolled*
+`dice.on('result', function (result, roller, game) {});` *when all the dice have been rolled*
 
-`dice.on('misroll', function (error, roller) {});` *if the dice was malformed or other errors occur*
+`dice.on('misroll', function (error, roller, game) {});` *if the dice was malformed or other errors occur*
 
 License
 -------
--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var events = require('events'),
 var Dice = function () {
     events.EventEmitter.call(this);
     var self = this;
-    this.roll = function (dice, roller) {
+    this.roll = function (dice, roller, game) {
         var roller = roller || 'player',
+            game = game || null,
             error = false,
             i, dices, times, withoutmod, result, mod, die;
         if (dice) {
-            self.emit('rolling', dice, roller);
+            self.emit('rolling', dice, roller, game);
             dices = dice.split('d');
             if (dices.length > 1) {
                 times = parseInt(dices[0]);
@@ -28,23 +29,23 @@ var Dice = function () {
                 if (!isNaN(die) && die > 0 && die < 1000001 && times > 0 && times < 1001) {
                     for (i = 0; i < times; i += 1) {
                         roll = rolling();
-                        self.emit('roll', roll, roller);
+                        self.emit('roll', roll, roller, game);
                         result += roll;
                     };
                 } else {
                     error = true;
-                    self.emit('misroll', 'A die must have 2 to 1M sides and you must roll 1 to 1k times', roller);
+                    self.emit('misroll', 'A die must have 2 to 1M sides and you must roll 1 to 1k times', roller, game);
                 }
             } else {
                 error = true;
-                self.emit('misroll', 'Malformed dice', roller);
+                self.emit('misroll', 'Malformed dice', roller, game);
             }
             if (result && !error) {
-                self.emit('result', result, roller);
+                self.emit('result', result, roller, game);
             }
         } else {
             error = true;
-            self.emit('misroll', 'No dice', roller);
+            self.emit('misroll', 'No dice', roller, game);
         }
     };
 };
